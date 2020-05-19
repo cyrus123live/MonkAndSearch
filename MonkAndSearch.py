e7d0c0ce0b4591036ea68fa3d05398f3041392e0
cyrus123live <64929788+cyrus123live@users.noreply.github.com>
2020-05-18 20:56:18 -0700

    Add files via upload

---
+++ MonkAndSearch.py
@@ -0,0 +1,38 @@
+def findNum(num, array):
+    l = 0
+    r = len(array) - 1
+    while(l < r):
+        mid = ( l + (r - 1) ) // 2
+        ##print("l = " + str(l) + " and r = " + str(r))
+        if(array[mid] == num):
+            return mid
+        elif(array[mid] < num):
+            l = mid + 1
+        elif(array[mid] > num):
+            r = mid - 1
+    return -1
+
+size = int(input())
+
+array = input().split(" ")
+intArray = []
+
+for i in range(size):
+    intArray.append(int(array[i]))
+
+numOfQueries = int(input())
+
+##for i in range(int(input())):
+
+for i in range(numOfQueries):
+    array = input().split(" ")
+    ## intArray.sort()
+    ## if (int(input[0]) == 0):
+    index = findNum(int(array[1]), intArray)
+    if (index != -1):
+        if (int(array[0]) == 0):
+            print(len(intArray) - index)
+        else:
+            print(len(intArray) - index - 1)
+    else:
+        print(0)
